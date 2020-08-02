53431ec251b42f775699cea770bbcf56dce60c7f
TheMerryMarlin <dam134@case.edu>
2020-08-01 20:33:04 -0500

    make this less dependent on where it runs

--- directory_monitor.py
+++ directory_monitor.py
@@ -9,13 +9,15 @@ def main(argv):
     path_to_watch = argv[0]
     before = dict([(f, None) for f in os.listdir(path_to_watch)])
     while 1:
-        time.sleep(60)
+        time.sleep(1)
         after = dict([(f, None) for f in os.listdir(path_to_watch)])
         added = [f for f in after if f not in before]
         if added:
             for new_file in added:
                 if new_file.endswith('.json'):
-                    race_parser.main(new_file)
+                    print("parsing: " + new_file)
+                    print(path_to_watch + new_file)
+                    race_parser.main(str(path_to_watch) + '\\' + str(new_file))
         before = after
 
 
